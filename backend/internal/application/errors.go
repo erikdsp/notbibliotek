e4f18db4c07a92b3de8233980886e1599b4b9485
Erik Dahl <erik@iunderlandet.se>
2026-09-08 13:59:03 +0200

    feat: add error types for file and score

--- backend/internal/application/errors.go
+++ backend/internal/application/errors.go
@@ -3,3 +3,5 @@ package application
 import "errors"
 
 var ErrSongNotFound = errors.New("song not found")
+var ErrFileNotFound = errors.New("file not found")
+var ErrScoreNotFound = errors.New("file not found")
