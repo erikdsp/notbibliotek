bd56899246d73381d2ea459d2ed5a75271de71a7
Erik Dahl <erik@iunderlandet.se>
2026-09-08 14:06:25 +0200

    feat: add score repository interface and postgres implementation

---
+++ backend/internal/application/score_repository.go
@@ -0,0 +1,11 @@
+package application
+
+import (
+	"github.com/erikdsp/notbibliotek/backend/internal/domain"
+	"github.com/oklog/ulid/v2"
+)
+
+type ScoreRepository interface {
+	Create(score domain.Score) error
+	GetByID(id ulid.ULID) (domain.Score, error)
+}
