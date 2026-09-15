3a22a70f92f68c2087b5a460e37d26008d166611
Erik Dahl <erik@iunderlandet.se>
2026-09-15 11:33:39 +0200

    feat: add Publish in handler and router

--- backend/internal/http/router.go
+++ backend/internal/http/router.go
@@ -19,6 +19,6 @@ func NewRouter(songHandler *SongHandler, songVersionHandler *SongVersionHandler)
 	mux.HandleFunc("PUT /api/v1/songs/{song_id}/versions/{version_id}/score", songVersionHandler.UpdateScore)
 	mux.HandleFunc("POST /api/v1/songs/{song_id}/versions/{version_id}/part/{key}", songVersionHandler.UploadPart)
 	mux.HandleFunc("PUT /api/v1/songs/{song_id}/versions/{version_id}/part/{key}", songVersionHandler.UpdatePart)
-
+	mux.HandleFunc("POST /api/v1/songs/{song_id}/versions/{version_id}/publish", songVersionHandler.Publish)
 	return mux
 }
